9933f15aa3d33254b6e5c007c57ade98ff57b1b1
giiulia <80638268+giiulia@users.noreply.github.com>
2023-05-21 16:01:36 +0200

    fit Criv con i tre parametri liberi

--- riv1/Is_Ceq/grafico_Criv.cpp
+++ riv1/Is_Ceq/grafico_Criv.cpp
@@ -17,11 +17,11 @@
 #include "TStyle.h"
 
 double funzioneC(double * x, double * par){
-	return sqrt(par[0]/(x[0]));
+	return par[1]+sqrt(par[0]/abs(x[0]+par[2]));
 }
 
 double funzioned(double * x, double * par){
-	return pow(10, 15)*8.85*pow(10, -12)*12*0.0009*sqrt(x[0]/par[0]);
+	return pow(10, 15)*8.85*pow(10, -12)*12*0.0009/(par[1]+sqrt(par[0]/abs(x[0]+par[2])));
 }
 
 using namespace std;
@@ -39,7 +39,6 @@ int main(int argc, char* argv[]){
   	while (!dati.eof()) {
  
      	dati >> Valim; // in V
-     	cout<<"Valim"<<Valim<<endl;
       	dati >> Ceq; // in pF
       	v_alim.push_back(Valim);
       	v_ceq.push_back(Ceq);
@@ -48,9 +47,9 @@ int main(int argc, char* argv[]){
   	
   	dati.close();
 
-//manipolazione dati ed errori
-  	double Ccavo_Cs [] = {128, 1.28*5}; // in pF 	ERRORE=10%
-  	double Cs[] = {4380, 43.8*5}; // in pF 
+//manipolazione dati ed errori (cambiando a 2% l'errore di TUTTE le misure vengono errori sulle Criv di decine di pF rispetto a 4pF di prima)
+  	double Ccavo_Cs [] = {128, 5}; // in pF 	ERRORE=2% invece di 3 pF 
+  	double Cs[] = {4380000, 200000}; // in pF 
   	double Ccavo[2];
 
 	vector<double> v_criv;
@@ -61,11 +60,11 @@ int main(int argc, char* argv[]){
 	double sigma_Ccavo = sqrt(  pow(Cs[1]*Ccavo_Cs[0]*Ccavo_Cs[0]/pow(Ccavo_Cs[0] - Cs[0], 2), 2) + pow(Ccavo_Cs[1]*Cs[0]*Cs[0]/pow(Ccavo_Cs[0] - Cs[0], 2), 2)  );
 	Ccavo[0] = valore_Ccavo;
 	Ccavo[1] = sigma_Ccavo;
-	cout<<"sigma_Ccavo: "<<sigma_Ccavo<<" pF"<<endl;
+	cout<<"Ccavo: "<<valore_Ccavo<< " +- "<<sigma_Ccavo<<" pF"<<endl;
   	for(int i = 0; i < v_alim.size(); i++){
   		v_criv.push_back(  pow(10, -3)*( (Ccavo[0]*(Cs[0]-v_ceq.at(i))-(Cs[0]*v_ceq.at(i)))/(v_ceq.at(i) - Cs[0]) )  ); // il 10^-3 è per ottenere i nF nel grafico
 
-      	v_criv_err.push_back( pow(10, -3)*sqrt(  pow((v_ceq.at(i)/20)*Cs[0]*Cs[0]/pow(v_ceq.at(i)-Cs[0], 2), 2) + pow(Ccavo[1], 2) + pow(Cs[1]*v_ceq.at(i)*v_ceq.at(i)/pow(v_ceq.at(i)-Cs[0], 2), 2)  ));
+      	v_criv_err.push_back( pow(10, -3)*sqrt(  pow(5*Cs[0]*Cs[0]/pow(v_ceq.at(i)-Cs[0], 2), 2) + pow(Ccavo[1], 2) + pow(Cs[1]*v_ceq.at(i)*v_ceq.at(i)/pow(v_ceq.at(i)-Cs[0], 2), 2)  ));
         cout<<"v_criv_err.at(i): "<<pow(10, -3)*sqrt(  pow(5*Cs[0]*Cs[0]/pow(v_ceq.at(i)-Cs[0], 2), 2) + pow(Ccavo[1], 2) + pow(Cs[1]*v_ceq.at(i)*v_ceq.at(i)/pow(v_ceq.at(i)-Cs[0], 2), 2)  ) << "nF" <<endl;
       	if(i < 8){
       		v_alim_err.push_back( sqrt(pow(0.01, 2)) );
@@ -77,9 +76,13 @@ int main(int argc, char* argv[]){
   	}
 
 //interpolazione
-	TF1 modelloC ("funzioneC", funzioneC, 0, 90, 1);
+	TF1 modelloC ("funzioneC", funzioneC, 0, 90, 3);
   	modelloC.SetParName(0, "k");
-	modelloC.SetParameter (0, 0.000023); 
+	modelloC.SetParameter (0, 11.5); 
+	modelloC.SetParName(1, "offset");
+	modelloC.SetParameter (1, 0.05);
+	modelloC.SetParName(2, "buffer");
+	modelloC.SetParameter (2, -0.00001);
 	
 //grafico
   	TCanvas c;
@@ -91,6 +94,10 @@ int main(int argc, char* argv[]){
   	g_Ceq.SetTitle(" ");
   	g_Ceq.SetMarkerStyle(20);
   	g_Ceq.SetMarkerSize(0.3);
+	g_Ceq.GetXaxis()->SetTitleSize(0.05);
+    g_Ceq.GetYaxis()->SetTitleSize(0.05);
+    g_Ceq.GetXaxis()->SetLabelSize(0.05);
+    g_Ceq.GetYaxis()->SetLabelSize(0.05);
 	g_Ceq.GetXaxis()->SetTitle("V al rivelatore [V]");
   	g_Ceq.GetYaxis()->SetTitle("C del rivelatore [nF]");
 
@@ -98,7 +105,7 @@ int main(int argc, char* argv[]){
 
  	g_Ceq.Draw("AP") ;
     
-    c.Print("Grafici/grafico_Ceq.pdf", "pdf") ; 
+    //c.Print("Grafici/grafico_Criv.pdf", "pdf") ; 
 
 //regione di svuotamento
 	vector<double> v_d, v_d_err;
@@ -111,9 +118,13 @@ int main(int argc, char* argv[]){
 	}
 
 //interpolazione regione di svuotamento
-	TF1 modellod ("funzioned", funzioned, 0, 90, 1);
+	TF1 modellod ("funzioned", funzioned, 0, 90, 3);
   	modellod.SetParName(0, "k");
-	modellod.SetParameter (0, 100); 
+	modellod.SetParameter (0, 11.5); 
+	modellod.SetParName(1, "offset");
+	modellod.SetParameter (1, 0.05);
+	modellod.SetParName(2, "drop");
+	modellod.SetParameter (2, -0.00001);
 
 //grafico regione di svuotamento
   	TCanvas c1;
@@ -125,6 +136,10 @@ int main(int argc, char* argv[]){
   	g_d.SetTitle(" ");
   	g_d.SetMarkerStyle(20);
   	g_d.SetMarkerSize(0.3);
+    g_d.GetXaxis()->SetTitleSize(0.05);
+    g_d.GetYaxis()->SetTitleSize(0.05);
+    g_d.GetXaxis()->SetLabelSize(0.05);
+    g_d.GetYaxis()->SetLabelSize(0.05);
 	g_d.GetXaxis()->SetTitle("V al rivelatore [V]");
   	g_d.GetYaxis()->SetTitle("regione di svuotamento [#mum]");
   
--- riv2/Is_Ceq/grafico_Criv.cpp
+++ riv2/Is_Ceq/grafico_Criv.cpp
@@ -17,11 +17,11 @@
 #include "TStyle.h"
 
 double funzioneC(double * x, double * par){
-	return sqrt(par[0]/x[0]);
+	return par[1]+sqrt(par[0]/abs(x[0]+par[2]));
 }
 
 double funzioned(double * x, double * par){
-	return pow(10, 6)*8.85*pow(10, -12)*2*sqrt(x[0]/par[0]);
+	return pow(10, 15)*8.85*pow(10, -12)*12*0.0009/(par[1]+sqrt(par[0]/abs(x[0]+par[2])));
 }
 
 using namespace std;
@@ -49,16 +49,25 @@ int main(int argc, char* argv[]){
   	dati.close();
 
 //manipolazione dati ed errori
-  	double Ccavo_Cs [] = {127.88, 3}; // in pF
+  	double Ccavo_Cs [] = {128, 5}; // in pF 
+  	double Cs[] = {4380000, 200000}; // in pF 
+  	double Ccavo[2];
+
 	vector<double> v_criv;
 	vector<double> v_criv_err, v_alim_err;
 
-  	for(int i = 0; i < v_alim.size(); i++){
-  		v_criv.push_back( pow(10, -3)*(v_ceq.at(i) - Ccavo_Cs[0]));
 
-      	v_criv_err.push_back( pow(10, -3)*sqrt(pow(3, 2) + pow(Ccavo_Cs[1], 2)) );
-        cout<<"v_criv_err.at(i): "<<pow(10, -3)*sqrt(pow(3, 2) + pow(Ccavo_Cs[1], 2)) << "nF" <<endl;
-      	if(i < 13){
+	double valore_Ccavo = Cs[0]*Ccavo_Cs[0]/(Cs[0] - Ccavo_Cs[0]);
+	double sigma_Ccavo = sqrt(  pow(Cs[1]*Ccavo_Cs[0]*Ccavo_Cs[0]/pow(Ccavo_Cs[0] - Cs[0], 2), 2) + pow(Ccavo_Cs[1]*Cs[0]*Cs[0]/pow(Ccavo_Cs[0] - Cs[0], 2), 2)  );
+	Ccavo[0] = valore_Ccavo;
+	Ccavo[1] = sigma_Ccavo;
+	cout<<"Ccavo: "<<valore_Ccavo<< " +- "<<sigma_Ccavo<<" pF"<<endl;
+  	for(int i = 0; i < v_alim.size(); i++){
+  		v_criv.push_back(  pow(10, -3)*( (Ccavo[0]*(Cs[0]-v_ceq.at(i))-(Cs[0]*v_ceq.at(i)))/(v_ceq.at(i) - Cs[0]) )  ); // il 10^-3 è per ottenere i nF nel grafico
+
+      	v_criv_err.push_back( pow(10, -3)*sqrt(  pow(5*Cs[0]*Cs[0]/pow(v_ceq.at(i)-Cs[0], 2), 2) + pow(Ccavo[1], 2) + pow(Cs[1]*v_ceq.at(i)*v_ceq.at(i)/pow(v_ceq.at(i)-Cs[0], 2), 2)  ));
+        cout<<"v_criv_err.at(i): "<<pow(10, -3)*sqrt(  pow(5*Cs[0]*Cs[0]/pow(v_ceq.at(i)-Cs[0], 2), 2) + pow(Ccavo[1], 2) + pow(Cs[1]*v_ceq.at(i)*v_ceq.at(i)/pow(v_ceq.at(i)-Cs[0], 2), 2)  ) << "nF" <<endl;
+      	if(i < 8){
       		v_alim_err.push_back( sqrt(pow(0.01, 2)) );
       	}
       	else{
@@ -68,9 +77,13 @@ int main(int argc, char* argv[]){
   	}
 
 //interpolazione
-	TF1 modelloC ("funzioneC", funzioneC, 0, 90, 1);
+	TF1 modelloC ("funzioneC", funzioneC, 0, 90, 3);
   	modelloC.SetParName(0, "k");
-	modelloC.SetParameter (0, 12.8); 
+	modelloC.SetParameter (0, 11.5); 
+	modelloC.SetParName(1, "offset");
+	modelloC.SetParameter (1, 0.05);
+	modelloC.SetParName(2, "drop");
+	modelloC.SetParameter (2, -0.00001);
 
 //grafico
   	TCanvas c;
@@ -82,6 +95,10 @@ int main(int argc, char* argv[]){
   	g_Ceq.SetTitle(" ");
   	g_Ceq.SetMarkerStyle(20);
   	g_Ceq.SetMarkerSize(0.3);
+  	g_Ceq.GetXaxis()->SetTitleSize(0.05);
+    g_Ceq.GetYaxis()->SetTitleSize(0.05);
+    g_Ceq.GetXaxis()->SetLabelSize(0.05);
+    g_Ceq.GetYaxis()->SetLabelSize(0.05);
 	g_Ceq.GetXaxis()->SetTitle("V al rivelatore [V]");
   	g_Ceq.GetYaxis()->SetTitle("C del rivelatore [nF]");
 
@@ -96,15 +113,20 @@ int main(int argc, char* argv[]){
 	double A = 0.0009; //900 mm^2 in m^2
 
 	for (int i = 0; i < v_alim.size(); i++){
-		v_d.push_back(  8.85*pow(10, -6)*2*A/(v_criv.at(i)*pow(10, -9))  ); //il 10^6 serve per ottenere dei micrometri
-		v_d_err.push_back(  pow(10, -3)*v_criv_err.at(i)*8.85*pow(10, -12)*2*A/pow((v_criv.at(i)*pow(10, -9)), 2)  );
+		v_d.push_back(  8.85*pow(10, -6)*12*A/(v_criv.at(i)*pow(10, -9))  ); //il 10^6 serve per ottenere dei micrometri
+		v_d_err.push_back(  pow(10, -3)*v_criv_err.at(i)*8.85*pow(10, -12)*12*A/pow((v_criv.at(i)*pow(10, -9)), 2)  );
 
 	}
 
 //interpolazione regione di svuotamento
-	TF1 modellod ("funzioned", funzioned, 0, 90, 1);
+	TF1 modellod ("funzioned", funzioned, 0, 90, 3);
   	modellod.SetParName(0, "k");
-	modellod.SetParameter (0, 12.8); 
+	modellod.SetParameter (0, 11.5); 
+	modellod.SetParName(1, "offset");
+	modellod.SetParameter (1, 0.05);
+	modellod.SetParName(2, "buffer");
+	modellod.SetParameter (2, -0.00001);
+	
 
 //grafico regione di svuotamento
   	TCanvas c1;
@@ -116,6 +138,10 @@ int main(int argc, char* argv[]){
   	g_d.SetTitle(" ");
   	g_d.SetMarkerStyle(20);
   	g_d.SetMarkerSize(0.3);
+  	g_d.GetXaxis()->SetTitleSize(0.05);
+    g_d.GetYaxis()->SetTitleSize(0.05);
+    g_d.GetXaxis()->SetLabelSize(0.05);
+    g_d.GetYaxis()->SetLabelSize(0.05);
 	g_d.GetXaxis()->SetTitle("V al rivelatore [V]");
   	g_d.GetYaxis()->SetTitle("regione di svuotamento [#mum]");
   
