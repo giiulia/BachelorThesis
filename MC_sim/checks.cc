e45ef03034406b38a147d7dce60da95dbf1d43c1
Giulia Cantarini <80638268+giiulia@users.noreply.github.com>
2023-09-27 17:55:57 +0200

    Update checks.cc

--- MC_sim/checks.cc
+++ MC_sim/checks.cc
@@ -41,17 +41,12 @@ double rand_range(double min, double max){
 
 double get_path (double x0, double y0, double theta, double phi, double S, double K_x, double K_y, double x_coinc, double y_coinc){
 
-    //std::cout<<"ang_theta: "<<theta<<"\t"<<"ang_phi: "<<phi<<std::endl;
-    //std::cout<<"S*K_x: "<<S*K_x<<"\t"<<"S*K_y: "<<S*K_y<<std::endl;
     double delta_x = x_coinc + S*K_x;
     double delta_y = y_coinc + S*K_y; 
-    //std::cout<<"y_coinc: "<<y_coinc<<"\t"<<"delta_y: "<<delta_y<<std::endl;
-    //std::cout<<"x_coinc: "<<x_coinc<<"\t"<<"delta_x: "<<delta_x<<std::endl;
     double complete = sqrt(pow(S*K_x, 2) + pow(S*K_y, 2) + pow(S, 2));
     double r = diametroRivelatore/2.;
     
     if(sqrt(pow(delta_x, 2) + pow(delta_y, 2)) > r){
-        //std::cout<<"QUIIII"<<std::endl;
         double h_out = (-sqrt(pow(K_x*r, 2) - pow(K_x*y_coinc, 2) + 2*K_x*K_y*x_coinc*y_coinc + pow(K_y*r, 2) - pow(K_y*x_coinc, 2)) - x_coinc*K_x - K_y*y_coinc)/(pow(K_x, 2) + pow(K_y, 2));
         std::cout<<"x0: "<<x0<<"\t"<<"y0: "<<y0<<std::endl;
         std::cout<<"ang_theta: "<<theta<<"\t"<<"ang_phi: "<<phi<<std::endl;
